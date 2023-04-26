39469cf5dd9a0de41ad44e2ad889bb21a9b32683
Andrea-Tornel <118896973+Andrea-Tornel@users.noreply.github.com>
2023-04-26 12:51:09 +0200

    Add files via upload

--- JuegoRPG/Player/Personaje.gd
+++ JuegoRPG/Player/Personaje.gd
@@ -4,13 +4,30 @@ const aceleracion = 500
 const velocidad_max = 80
 const friccion = 500
 
+enum {
+	MOVIMIENTO,
+	ATAQUE
+}
+
+var state = MOVIMIENTO
 var velocidad = Vector2.ZERO
 
 onready var animacionJugador = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
+func _ready():
+	animationTree.active = true
+
 func _physics_process(delta):
+	match state:
+		MOVIMIENTO:
+			movimiento_state(delta)
+		
+		ATAQUE:
+			ataque_state(delta)
+	
+func movimiento_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -20,6 +37,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Parar/blend_position", input_vector)
 		animationTree.set("parameters/Correr/blend_position", input_vector)
+		animationTree.set("parameters/Atacar/blend_position", input_vector)
 		animationState.travel("Correr")
 		velocidad = velocidad.move_toward(input_vector * velocidad_max, aceleracion * delta)
 		
@@ -28,3 +46,13 @@ func _physics_process(delta):
 		velocidad = velocidad.move_toward(Vector2.ZERO, friccion * delta)
 		
 	velocidad = move_and_slide(velocidad)
+	
+	if Input.is_action_just_pressed("Atacar"):
+		state = ATAQUE
+	
+func ataque_state(delta):
+	velocidad = Vector2.ZERO
+	animationState.travel("Atacar")
+
+func ataque_animation_finished():
+	state = MOVIMIENTO
