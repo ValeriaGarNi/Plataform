1f29fb4b271640488e45856d978187b3b06a1881
Andrea-Tornel <118896973+Andrea-Tornel@users.noreply.github.com>
2023-05-04 12:23:21 +0200

    Add files via upload

--- JuegoRPG/Player/Personaje.gd
+++ JuegoRPG/Player/Personaje.gd
@@ -32,7 +32,7 @@ func movimiento_state(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	print("velocidad")
+	#print("velocidad")
 	
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Parar/blend_position", input_vector)
